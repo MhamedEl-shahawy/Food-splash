5263569eb31f70730855211358c5954c344b3210
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-07-26 00:10:26 +0200

    add some code in script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const cardModel = document.getElementById("card");
 const closeElement = document.getElementById("close");
 const cards = document.querySelectorAll(".card");
+const vitamin_A =  document.getElementById("vitaminA");
+const vitamin_C =  document.getElementById("vitaminC");
+const iron =  document.getElementById("iron");
+const calcium =  document.getElementById("calcium");
+const card_title = document.getElementById("card-title");
+
+
+
+
 closeElement.onclick = () => cardModel.style.display = "none";
 cards.forEach(ele => {
       ele.onclick = () => cardModel.style.display = "block";
 });
+
+function fruits(vitaminA,vitaminC,calcium,iron,card_title){
+   
+     calcium.innerHTML = `${calcium} <span>%</span>`;
+     vitamin_C.innerHTML = `${vitaminC} <span>%</span>`;
+     vitamin_A.innerHTML = `${vitaminA} <span>%</span>`; 
+      iron.innerHTML = `${iron} <span>%</span>`;
+      card_title.innerHTML = `${card_title}`;
+}
