5731bf18fd0bcc1aab2c69ac96231cc0414479f9
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-07-26 22:07:17 +0200

    fix dom bug

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ const closeElement = document.getElementById("close");
 const cards = document.querySelectorAll(".card");
 const vitamin_A =  document.getElementById("vitaminA");
 const vitamin_C =  document.getElementById("vitaminC");
-const iron =  document.getElementById("iron");
-const calcium =  document.getElementById("calcium");
-const card_title = document.getElementById("card-title");
+
 
 
 
@@ -17,9 +15,9 @@ cards.forEach(ele => {
 
 function fruits(vitaminA,vitaminC,calcium,iron,card_title){
    
-     calcium.innerHTML = `${calcium} <span>%</span>`;
+      document.getElementById("calcium").innerHTML = `${calcium} <span>%</span>`;
      vitamin_C.innerHTML = `${vitaminC} <span>%</span>`;
      vitamin_A.innerHTML = `${vitaminA} <span>%</span>`; 
-      iron.innerHTML = `${iron} <span>%</span>`;
-      card_title.innerHTML = `${card_title}`;
+     document.getElementById("iron").innerHTML = `${iron} <span>%</span>`;
+      document.getElementById("card-title").textContent = `${card_title}`;
 }
